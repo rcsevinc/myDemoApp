ae768498b7f855c94cdf9bab5c1b8b75faea4884
Riza Can Sevinc <rcsevinc@gmail.com>
2017-09-25 19:17:40 +0000

    Demo files

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,16 +1,24 @@
 package com.mycompany.app;
 import java.util.*;
+
+
+import static spark.Spark.*;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-    public static boolean contains(ArrayList<String> list, String st1, String st2) {
+	public static boolean contains(ArrayList<String> list, String st1, String st2) {
     	
 		boolean temp1 = false;
 		boolean temp2 = false;
@@ -34,4 +42,56 @@ public class App
 		}
 		return (temp1 && temp2);
     }
+    public static void main( String[] args )
+    {
+        port(getHerokuAssignedPort());
+
+        get("/", (req, res) -> "Hello, World");
+
+        post("/compute", (req, res) -> {
+          //System.out.println(req.queryParams("input1"));
+          //System.out.println(req.queryParams("input2"));
+
+          String input1 = req.queryParams("input1");
+          java.util.Scanner sc1 = new java.util.Scanner(input1);
+          sc1.useDelimiter("[;\r\n]+");
+          java.util.ArrayList<String> inputList = new java.util.ArrayList<>();
+          while (sc1.hasNext())
+          {
+            String value = sc1.next().replaceAll("\\s","");
+            inputList.add(value);
+          }
+          System.out.println(inputList);
+
+
+          String input2 = req.queryParams("input2").replaceAll("\\s","");
+          String input3 = req.queryParams("input3").replaceAll("\\s","");
+          
+
+          boolean result = App.contains(inputList, input2, input3);
+
+         Map map = new HashMap();
+          map.put("result", result);
+          return new ModelAndView(map, "compute.mustache");
+        }, new MustacheTemplateEngine());
+
+
+        get("/compute",
+            (rq, rs) -> {
+              Map map = new HashMap();
+              map.put("result", "not computed yet!");
+              return new ModelAndView(map, "compute.mustache");
+            },
+            new MustacheTemplateEngine());
+
+    }
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+    
 }
